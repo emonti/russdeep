165633297bb3dc21f8e4f933cfb2d72ada87e631
Eric Monti <esmonti@gmail.com>
2011-01-28 17:12:37 -0600

    spec sample file helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,14 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'russdeep'
+require 'ssdeep'
 require 'spec'
 require 'spec/autorun'
 
 Spec::Runner.configure do |config|
   
 end
+
+def sample_file(fname)
+  File.expand_path(File.join(File.dirname(__FILE__), "samples", fname))
+end
+
