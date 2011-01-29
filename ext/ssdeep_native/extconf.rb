51a203d0e6dbe4b7d3aeaf07f61053cae524a8d8
Eric Monti <esmonti@gmail.com>
2011-01-28 18:38:52 -0600

    careful deps

--- ext/ssdeep_native/extconf.rb
+++ ext/ssdeep_native/extconf.rb
@@ -5,8 +5,11 @@ extension_name = "ssdeep_native"
 
 dir_config(extension_name)
 
-unless have_library("fuzzy") and
-       find_header("fuzzy.h", "/usr/local/include")
+unless find_library("fuzzy", "fuzzy_hash_file", "/lib", "/usr/lib", "/usr/local/lib") and
+       find_library("fuzzy", "fuzzy_hash_filename", "/lib", "/usr/lib", "/usr/local/lib") and
+       find_library("fuzzy", "fuzzy_hash_buf", "/lib", "/usr/lib", "/usr/local/lib") and
+       find_library("fuzzy", "fuzzy_compare", "/lib", "/usr/lib", "/usr/local/lib") and
+       find_header("fuzzy.h", "/lib", "/usr/lib", "/usr/local/include")
   raise "You must install the ssdeep package"
 end
 
